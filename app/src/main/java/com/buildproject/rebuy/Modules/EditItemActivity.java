5043a4fc96a872dcab297f1b206ce72a505af638
Liad Cohen <liad.code+git@gmail.com>
2019-12-11 18:02:18 +0200

    Added Edit Item activity, with design

---
+++ app/src/main/java/com/buildproject/rebuy/Modules/EditItemActivity.java
@@ -0,0 +1,16 @@
+package com.buildproject.rebuy.Modules;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.buildproject.rebuy.R;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+    }
+}
