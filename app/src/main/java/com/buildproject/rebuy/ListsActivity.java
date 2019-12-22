67f4a10a0140bbef2700121734e759200be82121
Liad Cohen <liad.code+git@gmail.com>
2019-12-22 22:04:57 +0200

    ListsActivity should now show all lists with populating data using another layout

--- app/src/main/java/com/buildproject/rebuy/ListsActivity.java
+++ app/src/main/java/com/buildproject/rebuy/ListsActivity.java
@@ -1,14 +1,44 @@
 package com.buildproject.rebuy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.buildproject.rebuy.Modules.ListOfItems;
+
+import java.util.List;
+
 public class ListsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycleview_lists);
+        new FirebaseDBadapter().readLists(new FirebaseDBadapter.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<ListOfItems> lists, List<String> keys) {
+                new RecyclerView_Config().setConfig(mRecyclerView, ListsActivity.this, lists,keys);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
     }
 }
