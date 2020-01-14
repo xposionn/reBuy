6453e65eaac66d7cbf2481088e8aba34e30972a0
Liad Cohen <liad.code+git@gmail.com>
2020-01-14 20:03:02 +0200

    Added camera Kit, photo taken with button, alert dialog ML Detector NOT IMPLEMENTED yet.

--- app/src/main/java/com/buildproject/rebuy/BarcodeScannerActivity.java
+++ app/src/main/java/com/buildproject/rebuy/BarcodeScannerActivity.java
@@ -2,16 +2,28 @@ package com.buildproject.rebuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
+
+import dmax.dialog.SpotsDialog;
 
 public class BarcodeScannerActivity extends AppCompatActivity {
 
     CameraView cameraView;
     Button btnDetectBarcode;
+    AlertDialog waitingDialog;
 
     @Override
     protected void onResume() {
@@ -34,5 +46,52 @@ public class BarcodeScannerActivity extends AppCompatActivity {
 
         cameraView = (CameraView) findViewById(R.id.camerview);
         btnDetectBarcode = (Button)findViewById(R.id.btn_capture_barcode);
+
+        waitingDialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Please wait..")
+                .setCancelable(false)
+                .build();
+
+        btnDetectBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraView.start();
+                cameraView.captureImage();
+            }
+        });
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                waitingDialog.show();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, cameraView.getWidth(),cameraView.getHeight(),false);
+                cameraView.stop();
+
+                runImageDetector(bitmap);
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+    }
+
+    //todo: Complete this function from documentation of VISION-ML - Google.
+    private void runImageDetector(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
     }
 }
